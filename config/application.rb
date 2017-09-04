be2bb66a979f000a2bbfe57661c06c4b9cc3f0c8
Shinichi Segawa <tektoh@gmail.com>
2017-09-04 18:14:50 +0900

    日本語環境の設定追加

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module StartDash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
