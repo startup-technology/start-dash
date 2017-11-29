499bd0c45c56d6e8e8758d7fb2f720a208713f9e
Shinichi Segawa <tektoh@gmail.com>
2017-11-29 17:24:52 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "start_dash_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
