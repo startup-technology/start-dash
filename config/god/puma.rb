f6de20ae7c667818d7e771dc3d3b52404d285719
Shinichi Segawa <tektoh@gmail.com>
2018-03-28 17:30:25 +0900

    god

---
+++ config/god/puma.rb
@@ -0,0 +1,19 @@
+RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + '/../')
+
+God.watch do |w|
+  w.name = "puma"
+
+  w.env = {
+    'RAILS_ROOT' => RAILS_ROOT,
+    'RAILS_ENV' => ENV['RAILS_ENV'] || 'development',
+    'RACK_ENV' => ENV['RACK_ENV'] || 'development'
+  }
+
+  w.start = "cd #{RAILS_ROOT} && bundle exec puma -C #{RAILS_ROOT}/config/puma.rb --daemon"
+  w.stop = "cd #{RAILS_ROOT} && bundle exec pumactl -S #{RAILS_ROOT}/tmp/pids/puma.state -F #{RAILS_ROOT}/config/puma.rb stop"
+  w.restart = "cd #{RAILS_ROOT} && bundle exec pumactl -S #{RAILS_ROOT}/tmp/pids/puma.state -F #{RAILS_ROOT}/config/puma.rb restart"
+
+  w.pid_file = File.join(RAILS_ROOT, "log/puma.pid")
+
+  w.keepalive
+end
---
+++ config/god/single.rb
@@ -0,0 +1,3 @@
+RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + '/../../')
+God.load "#{RAILS_ROOT}/config/god/puma.god"
+# God.load "#{RAILS_ROOT}/config/god/resque.god"
