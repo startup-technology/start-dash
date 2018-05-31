6164a55bad0c5e35294434a0a0d92514ee05f60e
Shinichi Segawa <tektoh@gmail.com>
2018-05-31 11:50:20 +0900

    CircleCIの設定を整理

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,15 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start 'rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
